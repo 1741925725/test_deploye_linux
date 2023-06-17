7bacdb743c68bdb2d48ae497c830d6734e4ea499
wangkang <1741925725@qq.com>
2023-06-17 15:47:44 +0800

    a

---
+++ src/main/java/com/example/test_deploy_linux/TestDeployLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_deploy_linux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployLinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeployLinuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_deploy_linux/TestDeployLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_deploy_linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployLinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
